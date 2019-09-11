d6bc3741fd43fd591a73fdcc43d83f313fa4ae12
Cyberdog90 <sumikaowata@gmail.com>
2019-09-11 23:41:26 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter.filedialog
+import os
+"""
+def main():
+    root = tkinter.Tk()
+    root.withdraw()
+    file_type = [("file", "*")]
+    idir = os.path.abspath(os.path.dirname(__file__))
+    filename = tkinter.filedialog.askopenfilename(filetypes=file_type,
+                                                  initialdir=idir)
+    with open(filename, "r") as f:
+        """
+
