5cda0463e1373ea093eb82e78129dbe8057dec50
Cyberdog90 <43635561+Cyberdog90@users.noreply.github.com>
2019-09-17 02:24:14 +0900

    up

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-with open("source.asm", "w") as f:
+import sys
+args = sys.argv
+with open("source.s", "w") as f:
     f.write(".intel_syntax noprefix\n")
     f.write(".global main\n")
     f.write("main:\n")
-    f.write("        mov rax, 42\n")
+    f.write("        mov rax, {}\n".format(args[1]))
     f.write("        ret\n")
 
     def main():
