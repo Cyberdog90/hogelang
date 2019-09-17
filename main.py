eecc61181da14b2170239b513aa153c1779ee1a5
Cyberdog90 <sumikaowata@gmail.com>
2019-09-18 01:30:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import sys
 
 args = sys.argv
 with open("source.s", "w") as f:
+    string = args[1]
     f.write(".intel_syntax noprefix\n")
     f.write(".global main\n")
     f.write("main:\n")
-
+    f.write("        mov rax, {}".format(strtol(string)))
 
     def main():
-        string = args[1]
+
         while True:
-            
+
 
 
         f.write("        mov rax, {}\n".format(0))
         f.write("        ret\n")
+
+    def strtol(arg):
+        for i in range(len(arg)):
+            if arg[0:i].isdecimal():
+                continue
+            return arg[0:i-1]
+
 
 
     if __name__ == '__main__':
